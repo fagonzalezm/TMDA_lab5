d22298ad31b9ca012abc7027d782432c7e032475
franco <franco.labra@usach.cl>
2021-07-19 02:57:00 -0400

    avance

--- lab5.r
+++ lab5.r
@@ -44,3 +44,58 @@ table(features$class)
 features.balanced <- downSample(x = features[, -ncol(features)],
                          y = features$class)
 table(features.balanced$Class)
+
+####### Se quita la variable cell shape ########
+
+features.balanced <- features.balanced[, -3]
+
+####### Seleccion de caracteristicas ###########
+
+ranking<-InfoGainAttributeEval(Class ~ . , data = features.balanced)
+print(ranking)
+
+# en base a esto se quita mitosis
+
+features.balanced <- features.balanced[, -8]
+
+ranking<-InfoGainAttributeEval(Class ~ . , data = features.balanced)
+print(ranking)
+
+
+################################
+########### SVM ################
+################################
+
+
+formula = features.balanced$Class ~ .
+model <- svm(formula, data = features.balanced)
+print(model)
+summary(model)
+
+
+x <- subset(features.balanced, select = -Class)
+y <- features.balanced$Class
+model <- svm(x, y)
+# test with train data
+pred <- predict(model, x)
+table(pred, y)
+
+# compute decision values and probabilities:
+pred <- predict(model, x, decision.values = TRUE)
+attr(pred, "decision.values")[1:7,]
+
+# visualize (classes by color, SV by crosses):
+plot(cmdscale(dist(features.balanced[,-8])), col = as.integer(features.balanced[,8]), pch = c("o","+")[1:478 %in% model$index + 1])
+
+
+obj <- tune(svm, Class ~ ., data = features.balanced, kernel = "linear", ranges = list(cost = 2^(-1:4)),tunecontrol = tune.control(sampling = "cross", cross = 2 ))
+summary(obj)
+
+plot(obj)
+summary(obj$best.model)
+
+pred <- predict(obj$best.model, x)
+table(pred, y)
+
+obj <- tune(svm, Class ~ ., data = features.balanced, kernel = "radial", ranges = list(gamma = 2^(-2:4), cost = 2^(-1:4), tunecontrol = tune.control(sampling = "cross", cross = 2 )))
+summary(obj)
